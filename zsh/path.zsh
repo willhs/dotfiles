359e47961fcb4725aaba4e25e199c1c1cb314b0b
Willhs <whardwicksmith@gmail.com>
2015-09-26 15:18:24 +1200

    ECS path set up

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,2 @@
-# add adb (android) to path
-export PATH=$PATH:/Users/willhs/Library/Android/sdk/platform-tools/
+# add uni paths
+export PATH=$PATH:$usr/pkg/qt4/bin:/usr/pkg/java/bin:/usr/pkg/java/sun-8/bin:/usr/local/bin:/usr/pkg/bin:/bin:/usr/bin:/usr/games
