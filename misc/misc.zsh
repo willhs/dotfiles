6b8f40767e39cfaf276e7c391d96aafec857e648
willhs <whardwicksmith@gmail.com>
2017-03-06 17:39:17 +1300

    xcape hhkb mapping, and loading a oh-my-zsh theme at the time that works

--- misc/misc.zsh
+++ misc/misc.zsh
@@ -1,1 +1,4 @@
+# mapping caps to escape
 xcape -e '#66=Escape'
+# mapping the HHKB star to super
+xcape -e '#102=Super_L'
--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,2 +1,4 @@
 # reloads the zsh theme (because it gets overwritten somewhere)
+local OHMYZSH=~/.oh-my-zsh
+local ZSH_THEME=robbyrussell
 source "$OHMYZSH/themes/$ZSH_THEME.zsh-theme"
