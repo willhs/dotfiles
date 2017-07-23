b79b435d8e4c7e02238ee046f2779ac78ead14ca
willhs <whardwicksmith@gmail.com>
2017-07-23 17:43:12 +1200

    tidied paths, added android and java home

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,11 +1,13 @@
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
+
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/willhs/.rvm/bin:/usr/lib/jvm/java-8-openjdk/bin"
+
+# texlive path
+export PATH="$PATH:/usr/local/texlive/2015basic/bin/x86_64-darwin/"
+
 # add adb (android) to path
 export PATH=$PATH:$HOME/Library/Android/sdk/platform-tools
 
-export ANDROID_HOME=$HOME/Library/Android/sdk
-export PI="202.150.114.180"
-
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/willhs/.rvm/bin"
-# texlive path
-export PATH="$PATH:/usr/local/texlive/2015basic/bin/x86_64-darwin/"
 # man path?
 export MANPATH="/usr/local/man:$MANPATH"
